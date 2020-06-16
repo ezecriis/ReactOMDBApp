e4cb0c8ebfa1abfd9622bad7aa6dc39815ee23c6
Cris <cr.ledesma@hotmail.com>
2020-06-16 16:14:12 -0300

    Finished proyect

---
+++ src/containers/List.js
@@ -0,0 +1,76 @@
+import React, { Fragment } from 'react';
+import Card from '../components/Card/Card';
+
+console.log(process.env.API);
+const API = process.env.API;
+
+class List extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            data: [],
+            searchTerm: '',
+            error: '',
+            loading: true,
+        }
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    async componentDidMount() {
+        // const res = await fetch('../../assets/data.json')
+        const res = await fetch(`${API}&s=batman`);
+        const resJSON = await res.json();
+        console.log(resJSON);
+        this.setState({ data: resJSON.Search, loading: false });
+    }
+
+    async handleSubmit(e) {
+        e.preventDefault();
+        if (!this.state.searchTerm) {
+            return this.setState({ error: 'Please white a valid text'})
+        }
+        const res = await fetch(`${API}&s=${this.state.searchTerm}`)
+        const data = await res.json();
+        if (!data.Search) {
+            return this.setState({error: 'There are no results'});
+        }
+        this.setState({ data: data.Search, error: '', searchTerm: '' })
+    }
+
+    render() {
+
+        const { data, loading } = this.state;
+        if (loading) {
+            return <h3 className="text-light">loading.. </h3>
+        }
+
+        return (
+            <Fragment>
+                <div className="row">
+                    <div className="col-md-4 offset-md-4 p-4">
+                    <form onSubmit={this.handleSubmit}>
+                        <input type="text"
+                        className="form-control"
+                        placeholder="Search"
+                        onChange={e => this.setState({searchTerm: e.target.value})}
+                        value={this.state.searchTerm}
+                        autoFocus
+                        />
+                    </form>
+                    <p className="text-white">{this.state.error ? this.state.error : ''}</p>
+                    </div>
+                </div>
+                <div className="row">
+                    {
+                        data.map((movie, i) => {
+                            return <Card movie={movie} key={i} />
+                        })
+                    }
+                </div>
+            </Fragment>
+        )
+    }
+}
+
+export default List;
